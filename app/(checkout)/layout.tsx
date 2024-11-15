d3d3ade68d9b0d4d73e0b57b97c1a2e3fca76719
Aldiyar <aldiyar@MacBook-Air-Aldiyar.local>
2024-11-15 20:59:19 +0500

    fix: add force-dynamic

--- app/(checkout)/layout.tsx
+++ app/(checkout)/layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 import { Container, Header } from '@/shared/components/shared';
 
 export const metadata = {
@@ -10,11 +10,13 @@ export default function CheckoutLayout({ children }: { children: ReactNode }) {
     return (
         <main className="min-h-screen bg-[#F4F1EE]">
             <Container>
-                <Header
-                    className="border-gray-200"
-                    hasSearch={false}
-                    hasCart={false}
-                />
+                <Suspense>
+                    <Header
+                        className="border-gray-200"
+                        hasSearch={false}
+                        hasCart={false}
+                    />
+                </Suspense>
                 {children}
             </Container>
         </main>
